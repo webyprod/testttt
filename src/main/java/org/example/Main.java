5bfc677757108d3786dd9498026490b9761f231f
fox <mk.alex1989@gmail.com>
2025-02-18 13:52:42 +0100

    initiation 9

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,7 @@ public class Main {
         List<Car> cars = createCarService.create();
         List<Car> cars2 = createCarService.create();
         List<Car> cars4 = createCarService.create();
+        List<Car> cars5 = createCarService.create();
         System.out.println(cars);
         System.out.println(cars);
         System.out.println(cars4);
@@ -30,6 +31,10 @@ public class Main {
         System.out.println(cars2);
         System.out.println(cars2);
         System.out.println(cars2);
+        System.out.println(cars5);
+        System.out.println(cars5);
+        System.out.println(cars5);
+        System.out.println(cars5);
 
         System.out.println(LocalDate.of(2025, 2, 10).isBefore(LocalDate.of(2025, 2, 4)));
     }
